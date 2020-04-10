24036dfdc040ecaf77c6ee918b598817a4351733
eyjhbb@gmail.com <eyjhbb@gmail.com>
2020-04-10 15:29:43 +0200

    remove commented code

--- registry/handlers/catalog.go
+++ registry/handlers/catalog.go
@@ -38,47 +38,4 @@ func (ch *catalogHandler) GetCatalog(w http.ResponseWriter, r *http.Request) {
 
 	paginateEndpoint(ch.Context, ps, w, r)
 	return
-
-	// 	var moreEntries = true
-
-	// 	q := r.URL.Query()
-	// 	lastEntry := q.Get("last")
-	// 	maxEntries, err := strconv.Atoi(q.Get("n"))
-	// 	if err != nil || maxEntries < 0 {
-	// 		maxEntries = maximumReturnedEntries
-	// 	}
-
-	// 	repos := make([]string, maxEntries)
-
-	// 	filled, err := ch.App.registry.Repositories(ch.Context, repos, lastEntry)
-	// 	_, pathNotFound := err.(driver.PathNotFoundError)
-
-	// 	if err == io.EOF || pathNotFound {
-	// 		moreEntries = false
-	// 	} else if err != nil {
-	// 		ch.Errors = append(ch.Errors, errcode.ErrorCodeUnknown.WithDetail(err))
-	// 		return
-	// 	}
-	// 	fmt.Println(filled, err, moreEntries)
-
-	// 	w.Header().Set("Content-Type", "application/json")
-
-	// 	// Add a link header if there are more entries to retrieve
-	// 	if moreEntries {
-	// 		lastEntry = repos[len(repos)-1]
-	// 		urlStr, err := createLinkEntry(r.URL.String(), maxEntries, lastEntry)
-	// 		if err != nil {
-	// 			ch.Errors = append(ch.Errors, errcode.ErrorCodeUnknown.WithDetail(err))
-	// 			return
-	// 		}
-	// 		w.Header().Set("Link", urlStr)
-	// 	}
-
-	// 	enc := json.NewEncoder(w)
-	// 	if err := enc.Encode(catalogAPIResponse{
-	// 		Repositories: repos[0:filled],
-	// 	}); err != nil {
-	// 		ch.Errors = append(ch.Errors, errcode.ErrorCodeUnknown.WithDetail(err))
-	// 		return
-	// 	}
 }
